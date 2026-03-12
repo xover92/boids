90ba9cba320254bb9aa7d3268e0b17c6dc249461
Yuri <delgiudiceyuri@gmail.com>
2026-03-12 18:25:08 +0100

    removed seaborn import

--- flock_statistics.py
+++ flock_statistics.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def compute_spatial_correlation(df_original, step, n_bins=50):
     # Filter the dataframe for the specific time step
